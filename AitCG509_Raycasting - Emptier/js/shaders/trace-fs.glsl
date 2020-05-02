a5aeba95511d3d070ea8213f97118a91ca920e08
Duc Trinh <duc.trinh.1501@gmail.com>
2020-05-03 05:43:22 +0700

    Started ray casting

--- AitCG509_Raycasting - Emptier/js/shaders/trace-fs.glsl
+++ AitCG509_Raycasting - Emptier/js/shaders/trace-fs.glsl
@@ -14,12 +14,42 @@ Shader.source[document.currentScript.src.split('js/shaders/')[1]] = `#version 30
     vec3 position;
   } camera;
 
+  float intersectQuadric(mat4 A, vec4 e, vec4 d){
+    float a = dot(d*A, d);
+    float b = dot(d*A, e) + dot(e*A, d);
+    float c = dot(e*A, e);
+    float disc = b*b-4.0*a*c;
+    if (disc < 0.0){
+      return -1.0;
+    }
+    else{
+      float t1 = (-1.0*b - sqrt(disc))/(2.0*a);
+      float t2 = (-1.0*b + sqrt(disc))/(2.0*a);
+      return (t1<0.0)?t2:((t2<0.0)?t1:min(t1, t2));
+    }
+  }
+
   void main(void) {
 	  vec4 e = vec4(camera.position, 1);		 //< ray origin
   	vec4 d = vec4(normalize(rayDir).xyz, 0); //< ray direction
 
-    // nothing hit by ray, return enviroment color
-	  fragmentColor = texture(material.envTexture, d.xyz );
+    mat4 A = mat4(1, 0, 0, 0,
+                  0, 4, 0, 0,
+                  0, 0, 0.5, 0,
+                  0, 0, 0, -9);
+
+    float t = intersectQuadric(A, e, d);
+
+    if (t < 0.0){
+      // nothing hit by ray, return enviroment color
+      fragmentColor = texture(material.envTexture, d.xyz );
+    }
+    else{
+      vec4 hit = e + d * t;
+      vec3 normal = normalize((hit * A + A * hit).xyz);
+      fragmentColor.rgb = normal;
+    }
+    
   }
 
 `;
